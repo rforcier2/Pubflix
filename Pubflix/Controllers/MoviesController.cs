31effe528402a4a1fbfb49eb447b856524539665
rforcier2 <rforcier2@gmail.com>
2018-08-20 09:38:44 -0400

    Updating Customers and Movies Controller, View, and Viewmodel to include our temporary hard coded customers. Also included a new view for login / register.

--- Pubflix/Controllers/MoviesController.cs
+++ Pubflix/Controllers/MoviesController.cs
@@ -9,7 +9,22 @@ using Pubflix.ViewModels;
 namespace Pubflix.Controllers
 {
     public class MoviesController : Controller
-    {
+    {	    
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+
         // GET: Movies / Random
         public ActionResult Random()
         {
